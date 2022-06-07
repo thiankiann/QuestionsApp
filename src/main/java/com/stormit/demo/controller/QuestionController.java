e380ac940a908f51d1ac7e612e1048ec047d0490
@thiankiann <thiankiann@gmail.com>
2022-06-07 19:46:21 +0100

    [add] Structure of CRUD implemented

--- src/main/java/com/stormit/demo/controller/QuestionController.java
+++ src/main/java/com/stormit/demo/controller/QuestionController.java
@@ -1,13 +1,27 @@
 package com.stormit.demo.controller;
 
 import com.stormit.demo.service.QuestionService;
-import com.stormit.demo.model.Questions;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.stormit.demo.model.Question;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
+/**
+ * CRUD implementation:
+ * C -create
+        ->POST/questions
+
+ * R -read
+        ->GET/questions       - for all questions
+        ->GET/questions/[id]  - for single question
+
+ * U -update
+        ->PUT/questions/[id]
+
+ * D -delete
+        ->DELETE/questions/[id]
+ */
 @RestController
 @RequestMapping("questions")
 public class QuestionController {
@@ -17,8 +31,27 @@ public class QuestionController {
     public QuestionController(QuestionService questionService) {
         this.questionService = questionService;
     }
+
     @GetMapping
-    List<Questions> getQuestions(){
+    List<Question> getQuestions(){
         return questionService.getQuestions();
     }
+    @GetMapping("{id}")
+    Question getQuestions(@PathVariable UUID id){
+        return questionService.getQuestions(id);
+    }
+    @PostMapping
+    Question createQuestion(@RequestBody Question question){
+        return questionService.createQuestion(question);
+    }
+    @PutMapping("{id}")
+    Question updateQuestion(@PathVariable UUID id, Question question){
+        return questionService.updateQuestion(id,question);
+    }
+    @DeleteMapping("{id}")
+    void deleteQuestion(@PathVariable UUID id){
+        questionService.deleteQuestion(id);
+    }
+
+
 }
--- src/main/java/com/stormit/demo/service/QuestionService.java
+++ src/main/java/com/stormit/demo/service/QuestionService.java
@@ -1,15 +1,31 @@
 package com.stormit.demo.service;
 
-import com.stormit.demo.model.Questions;
+import com.stormit.demo.model.Question;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class QuestionService {
 
-    public List<Questions> getQuestions() {
-        return Arrays.asList( new Questions("Question 1" ), new Questions("Question 2"));
+    public List<Question> getQuestions() {
+        return Arrays.asList( new Question("Question 1" ), new Question("Question 2"));
+    }
+
+    public Question getQuestions(UUID id) {
+        return null;
+    }
+
+    public Question createQuestion(Question question) {
+        return null;
+    }
+
+    public void deleteQuestion(UUID id) {
+    }
+
+    public Question updateQuestion(UUID id, Question question) {
+        return null;
     }
 }
