9fcd8352aa12af373aa298506fbf0aedb8298d97
@thiankiann <thiankiann@gmail.com>
2022-08-13 17:29:51 +0100

    Entity Manager Test presets - not working yet

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+drop table if exists accounts;
+create table accounts (
+                          id int,
+                          account_name varchar(255)
+);
---
+++ src/test/java/com/stormit/demo/postgres/transactions/tasksEntityManager.java
@@ -0,0 +1,49 @@
+package com.stormit.demo.postgres.transactions;
+
+import com.stormit.demo.postgres.entitymanager.Note;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+
+@SpringBootTest
+@Transactional
+public class tasksEntityManager {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @BeforeEach
+    void cleanUp() {
+        entityManager.createQuery("delete from Note").executeUpdate();
+    }
+
+    @Test
+    void shouldAddTwoRecords() {
+        // given
+        Note note1 = new Note();
+        note1.setId(1);
+        note1.setName("Note 1");
+        note1.setContent("Note 1 – content");
+
+        Note note2 = new Note();
+        note2.setId(2);
+        note2.setName("Note 2");
+        note2.setContent("Note 2 – content");
+
+        // when
+        entityManager.persist(note1);
+        entityManager.persist(note2);
+
+        // then
+        Long count = entityManager.createQuery("select count(n) from Note n", Long.class)
+                .getSingleResult();
+        assertThat(count).isEqualTo(___);
+    }
+}
