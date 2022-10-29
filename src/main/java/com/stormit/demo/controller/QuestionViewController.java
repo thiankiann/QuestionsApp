005b24850de8a081478be810a2c96b359f3954ef
@thiankiann <thiankiann@gmail.com>
2022-10-29 20:31:02 +0100

    QuestionViewController + thymeleaf - to improve

--- src/main/java/com/stormit/demo/controller/QuestionViewController.java
+++ src/main/java/com/stormit/demo/controller/QuestionViewController.java
@@ -1,10 +1,12 @@
 package com.stormit.demo.controller;
 
+import com.stormit.demo.model.Question;
 import com.stormit.demo.service.QuestionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.UUID;
@@ -14,23 +16,40 @@ import java.util.UUID;
 public class QuestionViewController {
 
     public QuestionService questionService;
+    //public Question question;
 
     public QuestionViewController(QuestionService questionService) {
         this.questionService = questionService;
     }
+   /* public QuestionViewController(QuestionService questionService, Question question) {
+        this.questionService = questionService;
+        this.question = question;
+    }*/
+
     @RequestMapping
     public String indexView(Model model){
         model.addAttribute("questions", questionService.getQuestions());
 
+     //   System.out.println("id = " + question.getId() );
         return "question/index";
     }
 
     @GetMapping("{id}")
-    public String singleView(@PathVariable UUID id){
+    public String singleView(Model model, @PathVariable UUID id){
+        model.addAttribute("question",questionService.getQuestion(id));
         return "question/single";
     }
+
     @GetMapping("add")
-    public String addView(){
+    public String addView(Model model){
+        model.addAttribute("add question",new Question() );
         return "question/add";
+    }
+
+    @PostMapping
+    public String add(Question question){
+        questionService.createQuestion(question);
+
+        return "redirect:/questions";
     }
 }
