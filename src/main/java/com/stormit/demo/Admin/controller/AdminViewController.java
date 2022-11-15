838194edcba23655c20829a155d8dfb6c0b178fa
@thiankiann <thiankiann@gmail.com>
2022-11-15 23:12:41 +0000

    admin

---
+++ src/main/java/com/stormit/demo/Admin/controller/AdminViewController.java
@@ -0,0 +1,15 @@
+package com.stormit.demo.Admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminViewController {
+    
+    @GetMapping
+    public String indexView(){
+        return "admin/index";
+    }
+}
