1dea2b9f3d3c257e21d8bd3f6fcd3a7d5b8dffb6
@thiankiann <thiankiann@gmail.com>
2022-11-17 10:23:55 +0000

    Admin - problem with packages sorted

---
+++ src/main/java/com/stormit/demo/Admin/controller/CategoryAdminViewController.java
@@ -0,0 +1,15 @@
+package com.stormit.demo.Admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/categories")
+public class CategoryAdminViewController {
+
+    @GetMapping
+    public String indexView(){
+        return "admin/category/index";
+    }
+}
