22fccfdb109d57896d28787709aa66b44781c2ee
@thiankiann <thiankiann@gmail.com>
2022-11-19 08:38:26 +0000

    admin/category -> tabela

--- src/main/java/com/stormit/demo/Admin/controller/CategoryAdminViewController.java
+++ src/main/java/com/stormit/demo/Admin/controller/CategoryAdminViewController.java
@@ -1,6 +1,8 @@
 package com.stormit.demo.Admin.controller;
 
+import com.stormit.demo.category.service.CategoryService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin/categories")
 public class CategoryAdminViewController {
 
+    private final CategoryService categoryService;
+
+    public CategoryAdminViewController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @GetMapping
-    public String indexView(){
+    public String indexView(Model model){
+        model.addAttribute("categories", categoryService.getCategories());
         return "admin/category/index";
     }
 }
--- src/main/java/com/stormit/demo/category/model/Category.java
+++ src/main/java/com/stormit/demo/category/model/Category.java
@@ -11,6 +11,7 @@ public class Category {
     }
 
     public Category(String name) {
+        this.id = UUID.randomUUID();
         this.name = name;
     }
 
