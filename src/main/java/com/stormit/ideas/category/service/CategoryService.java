56d6fd5e043ae869ddb840202c9d155f49644b99
@thiankiann <thiankiann@gmail.com>
2023-02-09 07:49:10 +0000

    Vaoidation CategoryAdminViewControler -> prototype

--- src/main/java/com/stormit/ideas/category/service/CategoryService.java
+++ src/main/java/com/stormit/ideas/category/service/CategoryService.java
@@ -2,11 +2,11 @@ package com.stormit.ideas.category.service;
 
 import com.stormit.ideas.category.domain.model.Category;
 import com.stormit.ideas.category.domain.repository.CategoryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -14,27 +14,23 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
+
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
 
     @Transactional(readOnly = true)
-    public List<Category> getCategories() {
-        return categoryRepository.findAll();
+    public Page<Category> getCategories(Pageable pageable) {
+        return getCategories(null, pageable);
     }
 
     @Transactional(readOnly = true)
-    public Category getCategory(UUID id) {
-        return categoryRepository.getById(id);
-    }
-
-    @Transactional
-    public Category createCategory(Category categoryRequest) {
-        Category category = new Category();
-
-        category.setName(categoryRequest.getName());
-
-        return categoryRepository.save(category);
+    public Page<Category> getCategories(String search, Pageable pageable) {
+        if(search == null) {
+            return categoryRepository.findAll(pageable);
+        } else {
+            return categoryRepository.findByNameContainingIgnoreCase(search, pageable);
+        }
     }
 
     @Transactional
