3b97d9798dbb1e0a972673b46f2af6b055d52248
@thiankiann <thiankiann@gmail.com>
2023-04-13 09:29:28 +0100

    Printing categories name in headlines ( CategoryViewController)

--- src/main/java/com/stormit/demo/category/controller/CategoryViewController.java
+++ src/main/java/com/stormit/demo/category/controller/CategoryViewController.java
@@ -2,8 +2,10 @@ package com.stormit.demo.category.controller;
 
 import com.stormit.demo.category.domain.model.Category;
 import com.stormit.demo.category.service.CategoryService;
+import com.stormit.demo.common.controller.IdeasCommonViewController;
 import com.stormit.demo.question.domain.model.Question;
 import com.stormit.demo.question.service.QuestionService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,15 +17,11 @@ import java.util.UUID;
 
 @Controller
 @RequestMapping("/categories")
-public class CategoryViewController {
+@RequiredArgsConstructor
+public class CategoryViewController extends IdeasCommonViewController {
 
     private final CategoryService categoryService;
     private final QuestionService questionService;
-
-    public CategoryViewController(CategoryService categoryService, QuestionService questionService) {
-        this.categoryService = categoryService;
-        this.questionService = questionService;
-    }
 
     @GetMapping("{id}")
     public String singleView(@PathVariable UUID id, Model model){
@@ -32,6 +30,7 @@ public class CategoryViewController {
 
         model.addAttribute("category", category);
         model.addAttribute("questions", questions);
+        addGlobalAttributes(model);
 
         return "category/single";
     }
